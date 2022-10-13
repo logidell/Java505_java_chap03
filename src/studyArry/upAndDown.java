59bfe1b2f932e9439d2edc6d0954779050cd6ada
DESKTOP-OHBVMKI\admin <yuhiee123@gmail.com>
2022-10-13 15:55:48 +0900

    file move

---
+++ src/studyArry/upAndDown.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class upAndDown {
+
+    public static void main(String[] args) {
+
+        System.out.println("Up & Down! \n 숨겨진 수를 맞춰주세요.");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        Random random = new Random();
+//
+    }
+
+
+}
+
+
+
